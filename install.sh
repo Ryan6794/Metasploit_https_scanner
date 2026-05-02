77b7e42c265a6c02c8540a69c8d8c9eb92e2c3db
Ryan Lyman <ryanlyman@gmail.com>
2026-05-02 14:26:25 -0600

    subdomain path fix

--- install.sh
+++ install.sh
@@ -8,10 +8,16 @@ echo "[*] Installing HTTP/HTTPS Subdomain Scanner..."
 MODULE_URL="https://raw.githubusercontent.com/Ryan6794/Metasploit_https_scanner/main/https_subdomain_scanner.rb"
 WORDLIST_URL="https://raw.githubusercontent.com/Ryan6794/Metasploit_https_scanner/main/common.txt"
 
+# --- DETECT METASPLOIT INSTALL TYPE ---
+if [ -d "/usr/share/metasploit-framework" ]; then
+    MSF_BASE="/usr/share/metasploit-framework"
+else
+    MSF_BASE="$HOME/.msf4"
+fi
+
 # --- PATHS ---
-MSF_DIR="$HOME/.msf4"
-MODULE_DIR="$MSF_DIR/modules/auxiliary/scanner/http"
-WORDLIST_DIR="$MSF_DIR/data/subdomains"
+MODULE_DIR="$MSF_BASE/modules/auxiliary/scanner/http"
+WORDLIST_DIR="$MSF_BASE/data/subdomains"
 
 # --- CREATE DIRECTORIES ---
 echo "[*] Creating directories..."
@@ -42,7 +48,9 @@ else
     echo "[!] Wordlist install failed"
 fi
 
-# --- DONE ---
+echo ""
+echo "[*] Installation complete"
+echo "[*] Metasploit base detected at: $MSF_BASE"
 echo ""
 echo "[*] Launch Metasploit:"
 echo "    msfconsole"
@@ -51,5 +59,5 @@ echo "[*] Then run:"
 echo "    reload_all"
 echo "    use auxiliary/scanner/http/https_subdomain_scanner"
 echo ""
-echo "[*] Optional:"
-echo "    set SUBDOMAIN_FILE ~/.msf4/data/subdomains/common.txt"
+echo "[*] Default wordlist path will be:"
+echo "    $WORDLIST_DIR/common.txt"
